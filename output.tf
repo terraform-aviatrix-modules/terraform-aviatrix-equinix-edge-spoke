5d82c0f6a5ae4d3cc7e5ce780ad426cfb9f922a3
Dennizz <root@ipaddr.nl>
2024-12-18 15:08:39 +0100

    add outputs

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "ha_management_ip" {
 output "equinix_network_device" {
   value = equinix_network_device.default
 }
+
+output "aviatrix_edge_equinix" {
+  value = aviatrix_edge_equinix.default
+}
+
+output "aviatrix_edge_equinix_ha" {
+  value = var.ha_gw ? aviatrix_edge_equinix_ha.default : null
+}
